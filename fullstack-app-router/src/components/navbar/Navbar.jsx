ec1e700558b4637b7f2551b26a3a9d8b0e43b107
iRobot90 <amiandajesse27@gmail.com>
2024-07-14 17:16:16 +0300

    pages

--- fullstack-app-router/src/components/navbar/Navbar.jsx
+++ fullstack-app-router/src/components/navbar/Navbar.jsx
@@ -1,51 +1,60 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css"
+
 
 const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio", // portfolio section has a URL of /portfolio
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog", //blog section has a URL of /blog
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about", //  about section has a URL of /about
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact", //  contact section has a URL of /contact
-    },
-    {
-      id: 6,
-      title: "Dashboard", //  dashboard section has an ID of 6 and URL of /dashboard
-      url: "/dashboard",
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div>
-        <Link href='/'>Amiandah</Link>
-    <div>
-        {links.map(link=>
-            <Link key={link.id} href={link.url}>{link.title}</Link>
-        )}
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Amiandah
+      </Link>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+          <button className={styles.logout}>           Logout
+          </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
